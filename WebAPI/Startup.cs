a0a913e3b73f9cb1fc061c94366fba3e07605b79
Vlad <poenaru.vlad98@yahoo.com>
2021-06-29 22:10:16 +0300

    hosted to azure

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -71,6 +71,9 @@ namespace WebAPI
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
